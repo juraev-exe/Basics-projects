40dfb6cf7596243959912c8b4539d3898bc16ee3
JA <abubakrjuraevv@gmail.com>
2026-05-10 08:19:28 -0700

    Update C++ build configuration and add template for competitive programming

---
+++ template.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// Speed-up for competitive programming
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+// PBDS (Policy Based Data Structures) - Ordered Set
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+void solve() {
+    // Your code here
+}
+
+int main() {
+    FAST_IO
+    int t = 1;
+    // cin >> t; // Uncomment if there are multiple test cases
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
