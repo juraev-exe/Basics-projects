33eec41a5d3e623e8b154f48b8e46b9535afc61f
JA <abubakrjuraevv@gmail.com>
2026-09-23 07:56:10 +0500

    Fix typo in perimeter calculation output in test.cpp

--- test.cpp
+++ test.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-
+    
     int a,b;
-    cout << "Enter the length of side 1: ";
+    cout << "Enter the length of side 1";
     cin >> a ;
-    cout << "Enter the length of side 2: ";
+    cout << "Enter the length of side 2";
     cin >> b ;
-    cout << "The perimetr is " << a*b << endl;
+    cout << "The perimetr is " << a*b << endl; 
     system("pause");
     return 0;
 }
